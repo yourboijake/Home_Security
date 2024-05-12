badb2acfe9d4e1969ee9d8e626f011bc670871f2
yourboijake <44953097+yourboijake@users.noreply.github.com>
2024-05-12 15:11:58 -0600

    Update smtp.py

--- smtp.py
+++ smtp.py
@@ -1,21 +1,2 @@
 import smtplib
  
-# creates SMTP session
-s = smtplib.SMTP('smtp.gmail.com', 587)
- 
-# start TLS for security
-s.starttls()
- 
-# Authentication
-s.login("jakeydunning98@gmail.com", "DutchBitches!")
- 
-# message to be sent
-message = "test email"
- 
-# sending the mail
-s.sendmail("jakeydunning98@gmail.com", 
-           "jacob_dunning@outlook.com", 
-           message)
- 
-# terminating the session
-s.quit()
