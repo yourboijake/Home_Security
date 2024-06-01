2b5bd5e6dfa5033405519923fc2318b2c8f04f8d
yourboijake <jakeydunning98@gmail.com>
2024-06-01 10:56:51 -0600

    set up cam streaming

--- README.md
+++ README.md
@@ -9,10 +9,16 @@ Hardware requirements:
 Software installation steps:
 
 
-TODO:
+Dev TODO:
 - add pw protection to home page: https://www.digitalocean.com/community/tutorials/how-to-add-authentication-to-your-app-with-flask-login
-- set up camera script to capture video and scp to server
-- set up REST API on server to toggle camera streaming
-- add motion detection algorithm
+- set up REST API on server to toggle camera streaming: https://www.linode.com/docs/guides/create-restful-api-using-python-and-flask/
+- motion detection algorithm
 - add SMTP to send emails on motion capture
 - add S3 connection to save images (renaming the files with current ts)
+
+
+Testing TODO:
+- set up camera script to capture video and scp to server
+
+Finished Testing:
+ 
--- camera/cam.py
+++ camera/cam.py
@@ -1,6 +1,10 @@
 from utils import capture_image, send_image, toggle_scp
 import os
 import time
+from datetime import datetime
+import json
+import cv2
+import logging
 
 '''
 main function that captures images and sends to remote server
@@ -11,3 +15,27 @@ the main loop is as follows:
 - if false, continue iterating until next API call
 
 '''
+
+#load in credentials
+api_cred = json.load(open('api_cred.json'))
+scp_cred = json.load(open('scp_cred.json'))
+
+#camera setup and settings
+cv2_camera = cv2.VideoCapture(0)
+FRAMERATE = 2  #frames per second
+STREAM_TOGGLE = False #send frames or not, boolean
+
+start_ts = datetime.now()
+while True:
+    #define API call logic here
+
+    if STREAM_TOGGLE == True:
+        img_fname = capture_image(cv2_camera=cv2_camera)
+        process = send_image(img_fname=img_fname)
+        if process.returncode != 0:
+            open('err.log', 'a').write(process.stderr)
+            logging.warning('Failed to scp file to remote server')
+
+        time.sleep(1.0 / FRAMERATE)
+    else:
+        time.sleep(1)
